2c864bcb1d188964f6535bd82fc411d9d38c68d0
infomon <simon.schrodi09@gmail.com>
2020-01-31 15:10:10 +0100

    remove unnecessary configs and normalize

--- utils.py
+++ utils.py
@@ -14,17 +14,45 @@ def make_torch_dataset(data, targets):
 def make_torch_dataloader(torch_dataset,batch_size):
     return DataLoader(dataset=torch_dataset,batch_size=batch_size)
 
-def remove_config_entry(configs,key):
+def remove_config_entry(configs,keys=['activation',
+                                      'cosine_annealing_T_max',
+                                      'cosine_annealing_eta_min',
+                                      'imputation_strategy',
+                                      'learning_rate_scheduler',
+                                      'loss',
+                                      'mlp_shape',
+                                      'normalization_strategy',
+                                      'optimizer']):
     for c in configs:
-      if key in c.keys():
-        del c[key]
+        for key in keys:
+            if key in c.keys():
+                del c[key]
     return configs
 
 def get_first_n_epochs(temporal_data,n=10):
     return temporal_data[:,:n]
 
-def extract(data, key):
+def extract_from_data(data, key):
     output = []
     for d in data:
         output.append(d[key])
     return np.array(output)
+
+def normalize_configs(configs):
+    for config in configs:
+        #config['activation'] = 0 if "relu" else 1
+        config['batch_size'] /= 511
+        #config['cosine_annealing_T_max'] = 1
+        #config['cosine_annealing_eta_min'] = config['cosine_annealing_eta_min']
+        #config['imputation_strategy'] = 0
+        config['learning_rate'] = config['learning_rate']
+        #config['learning_rate_scheduler'] = 0
+        #config['loss'] = 0
+        config['max_dropout'] = config['max_dropout']
+        config['max_units'] /= 1024
+        #config['mlp_shape'] = 0
+        config['momentum'] = config['momentum']
+        #config['normalization_strategy'] = 0
+        config['num_layers'] /= 4
+        #config['optimizer'] = 0
+        config['weight_decay'] = config['weight_decay']
