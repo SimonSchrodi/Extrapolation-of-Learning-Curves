485d4100e92626222c5a3f9b4e39c522cc6f547e
infomon <simon.schrodi09@gmail.com>
2020-02-26 10:00:44 +0100

    Pre-final poster version and adjusted boxplot script

--- boxplots.py
+++ boxplots.py
@@ -9,13 +9,55 @@ plt.rcParams['text.latex.preamble'] = [r"\usepackage{amsmath}"]
 # Fixing random state for reproducibility
 np.random.seed(19680801)
 
-default = np.load(r'Default/default_max_errors.npy')
-mlp_cond = np.load(r'PreConditional_LSTMs/max_errors.npy')
-pre_cond = np.load(r'PreConditional_LSTMs/max_errors.npy')
-inter_cond = np.load(r'InterConditional_LSTMs/max_errors.npy')
-post_cond = np.load(r'restricted_(Post)Conditional_RNN_models/mean_max_list/mean_max_list_rnn_simpliefied.npy')
-post_cond_na = np.load(r'optimized_(Post)Conditional_RNN_models/mean_max_list/mean_max_list_rnn1_optimized.npy')
-enc_dec = np.load(r'Encoder-Decoder-Models/max_list_not_abs/mean_max_list_encoder_decoder4.npy')
+averaged=False
+if averaged:
+    default = np.load(r'Default/default_max_errors.npy')
+    mlp_cond = np.load(r'PreConditional_LSTMs/max_errors.npy')
+    pre_cond = np.load(r'PreConditional_LSTMs/max_errors.npy')
+    inter_cond = np.load(r'InterConditional_LSTMs/max_errors.npy')
+    post_cond = np.load(r'restricted_(Post)Conditional_RNN_models/mean_max_list/mean_max_list_rnn_simpliefied.npy')
+    post_cond_na = np.load(r'optimized_(Post)Conditional_RNN_models/mean_max_list/mean_max_list_rnn1_optimized.npy')
+    enc_dec = np.load(r'Encoder-Decoder-Models/max_list_not_abs/mean_max_list_encoder_decoder4.npy')
+else:
+    default = np.load(r'Default/default_max_errors.npy')
+    default = np.stack([default for _ in range(5)])
+    default = default.reshape((-1))
+
+    mlp_cond = []
+    mlp_cond.append(np.load(r'DL_PROJECT_FREIBURG/error_signed_model_0.npy'))
+    mlp_cond.append(np.load(r'DL_PROJECT_FREIBURG/error_signed_model_1.npy'))
+    mlp_cond.append(np.load(r'DL_PROJECT_FREIBURG/error_signed_model_2.npy'))
+    mlp_cond.append(np.load(r'DL_PROJECT_FREIBURG/error_signed_model_3.npy'))
+    mlp_cond.append(np.load(r'DL_PROJECT_FREIBURG/error_signed_model_4.npy'))
+    mlp_cond = np.array(mlp_cond).reshape((-1))
+
+    pre_cond = np.load(r'PreConditional_LSTMs/all_signed_errors.npy').reshape((-1))
+    inter_cond = np.load(r'InterConditional_LSTMs/all_signed_errors.npy').reshape((-1))
+
+    post_cond = []
+    post_cond.append(np.load(r'restricted_(Post)Conditional_RNN_models/max_list_not_abs/max_list_rnn0.npy'))
+    post_cond.append(np.load(r'restricted_(Post)Conditional_RNN_models/max_list_not_abs/max_list_rnn1.npy'))
+    post_cond.append(np.load(r'restricted_(Post)Conditional_RNN_models/max_list_not_abs/max_list_rnn2.npy'))
+    post_cond.append(np.load(r'restricted_(Post)Conditional_RNN_models/max_list_not_abs/max_list_rnn3.npy'))
+    post_cond.append(np.load(r'restricted_(Post)Conditional_RNN_models/max_list_not_abs/max_list_rnn4.npy'))
+    post_cond = np.array(post_cond).reshape((-1))
+
+    post_cond_na = []
+    post_cond_na.append(np.load(r'optimized_(Post)Conditional_RNN_models/max_list_not_abs/max_list_not_abs0.npy'))
+    post_cond_na.append(np.load(r'optimized_(Post)Conditional_RNN_models/max_list_not_abs/max_list_not_abs1.npy'))
+    post_cond_na.append(np.load(r'optimized_(Post)Conditional_RNN_models/max_list_not_abs/max_list_not_abs2.npy'))
+    post_cond_na.append(np.load(r'optimized_(Post)Conditional_RNN_models/max_list_not_abs/max_list_not_abs3.npy'))
+    post_cond_na.append(np.load(r'optimized_(Post)Conditional_RNN_models/max_list_not_abs/max_list_not_abs4.npy'))
+    post_cond_na = np.array(post_cond_na).reshape((-1))
+
+    enc_dec = []
+    enc_dec.append(np.load(r'Encoder-Decoder-Models/max_list_not_abs/max_list_encoder_decoder_notabs0.npy'))
+    enc_dec.append(np.load(r'Encoder-Decoder-Models/max_list_not_abs/max_list_encoder_decoder_notabs1.npy'))
+    enc_dec.append(np.load(r'Encoder-Decoder-Models/max_list_not_abs/max_list_encoder_decoder_notabs2.npy'))
+    enc_dec.append(np.load(r'Encoder-Decoder-Models/max_list_not_abs/max_list_encoder_decoder_notabs3.npy'))
+    enc_dec.append(np.load(r'Encoder-Decoder-Models/max_list_not_abs/max_list_encoder_decoder_notabs4.npy'))
+    enc_dec = np.array(enc_dec).reshape((-1))
+
 # Making a 2-D array only works if all the columns are the
 # same length.  If they are not, then use a list instead.
 # This is actually more efficient because boxplot converts
@@ -33,10 +75,10 @@ ax.set_xticklabels(np.repeat(['Default','MLP','Pre-Cond. RNN','Inter-Cond. RNN',
                                  ,'Pre-Cond. Enc.-Dec.'], 1),
                     rotation=30, fontsize=13)
 ax.set_axisbelow(True)
-ax.hlines(0,0,7,linestyles='--')
+ax.hlines(0,0,8,linestyles='--')
 
 
-fig.text(0.75, 0.94,r'Variance of models', color='black', weight='roman',fontsize=12)
+fig.text(0.75, 0.94,r'Std. dev. of models', color='black', weight='roman',fontsize=12)
 fig.text(0.75, 0.91,('Default\t\t\t\t\t'+str(round(np.std(default),2))).expandtabs(), color='black', weight='roman',size='x-small')
 fig.text(0.75, 0.88,('MLP\t\t\t\t\t'+str(round(np.std(mlp_cond),2))).expandtabs(), color='black', weight='roman',size='x-small')
 fig.text(0.75, 0.85,('Pre-Cond. RNN\t\t\t '+str(round(np.std(pre_cond),2))).expandtabs(),color='black', weight='roman', size='x-small')
